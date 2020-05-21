199a02dbd23efdbc2d3af6612c7a34c47cad16ee
Frank Flores <frankthetank67@gmail.com>
2020-05-21 14:09:08 -0700

    added database files, config folder with connection to mysql, and a server.js file requiring express and express-handlebars

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Western Bacon', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Buffalo Burger', false);
