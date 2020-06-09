9b4dbd75e0ec1c76712629c77eb9216f4d9e261c
fmflores <frankthetank67@gmail.com>
2020-06-08 23:14:01 -0700

    added selectAll, insertOne, and updateOne methods in orm.js and exported orm file

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,66 @@
 var connection = require("../config/connection.js")
 
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (let i = 0; i < num; i++) {
+        arr.push('?');
+    }
+
+    return arr.toString();
+}
+
+function objToSql(obj) {
+    var arr = [];
+
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            arr.push(key + '=' = obj[key]);
+        }
+    }
+    return arr.toString();
+}
+
+const orm = {
+    selectAll: function (tableInput, cb) {
+        const queryString = 'SELECT * FROM ' + tableInput + ';';
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+    insertOne: function (table, cols, vals, cb) {
+        const queryString = 'INSERT INTO ' + table;
+        queryString += ' (';
+        queryString += cols.toString();
+        queryString += ') ';
+        queryString += 'VALUES (';
+        queryString += printQuestionMarks(vals.length);
+        queryString += ') ';
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+
+    updateOne: function (tble, objColVals, condition, cb) {
+        const queryString = 'UPDATE ' + table;
+
+        queryString += ' SET ';
+        queryString += objToSql(objColVals);
+        queryString += ' WHERE ';
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    }
+
+}
+
+module.exports = orm;
